a2f3f62d5ff6c0c4970601ad6a52b51ef24868a4
lararf05 <164400685+lararf05@users.noreply.github.com>
2024-10-04 01:47:25 -0300

    Add files via upload

---
+++ Questao1/src/Main.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Funcionario f = new Funcionario();
+        Gerente g = new Gerente();
+        Programador p = new Programador();
+
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
+        String r;
+
+        do{System.out.println("Digite 0 se for Programador e 1 se for Gerente");
+            int opcao = scanner.nextInt();
+
+            System.out.println("Digite o nome do Funcionario: ");
+            f.setNome(scanner2.nextLine());
+
+            System.out.println("Digite o ano de nascimento: ");
+            f.setNascimento(scanner.nextInt());
+
+            System.out.println("Digite o salario do Funcionario: ");
+            f.setSalario(scanner.nextDouble());
+
+
+            switch(opcao){
+                case(0):
+                    p.informarLinguagem();
+                    break;
+
+                case(1):
+                    g.informarProjeto();
+                    break;
+
+            }
+
+            System.out.println("Nome: "+f.getNome());
+            f.calcularIdade();
+            System.out.println("Idade: "+f.getIdade());
+            System.out.println("Salario: "+f.getSalario());
+            if(opcao==0){
+                System.out.println("Linguagem: " + p.getLinguagem());
+            }else{
+                System.out.println("Projeto "+ g.getProjeto());
+            }
+
+            System.out.println("Deseja cadastrar outro usuário, digite S");
+            r = scanner.next();
+
+        }while (r.equalsIgnoreCase("S"));
+
+    }
+}
